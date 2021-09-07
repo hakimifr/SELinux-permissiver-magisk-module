c32b5567bbe42c5d4c472a55bcbf0e60b2154da0
Hakimi <67860246+Hakimi0804@users.noreply.github.com>
2021-09-06 22:35:32 -0400

    Update script1.sh

--- common/script1.sh
+++ common/script1.sh
@@ -1,3 +1,3 @@
-while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
+while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done # stolen from YouTube Vanced script
 sleep 1
 setenforce 0
